06f28ec89511446d47bc8378cf9f9a7add9732f6
Parkprogrammer <127393724+Parkprogrammer@users.noreply.github.com>
2023-05-22 22:05:44 +0900

    Add files via upload

---
+++ term_P/src/Obj/obj.java
@@ -0,0 +1,11 @@
+package Obj;
+
+import java.awt.image.BufferedImage;
+
+public class obj {
+	public int x,y;
+	public int speed;
+	public String direction;
+	public boolean collisionOn;
+	public BufferedImage image;
+}
